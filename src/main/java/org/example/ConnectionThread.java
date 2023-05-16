52eda9d12cd43cb3cbff2b569c3a3fe23f250d94
Mateusz Murawski <303840@office.umcs.pl>
2023-05-16 15:28:41 +0200

    cc

--- src/main/java/org/example/ConnectionThread.java
+++ src/main/java/org/example/ConnectionThread.java
@@ -1,4 +1,36 @@
 package org.example;
 
+import java.io.*;
+import java.net.Socket;
+
 public class ConnectionThread extends Thread{
+    private Socket socket;
+    private PrintWriter writer;
+
+    public ConnectionThread(String address, int port) throws IOException {
+        socket = new Socket(address,port);
+        System.out.println("Plase login");
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream input  = socket.getInputStream();
+            OutputStream output = socket.getOutputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            this.writer = new PrintWriter(output, true);
+
+            String message;
+            while ((message = reader.readLine()) != null) {
+                System.out.println(message);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendMessage(String message){
+        writer.println(message);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            ConnectionThread connection = new ConnectionThread("localhost",5000);
+            connection.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while(true){
+                String message = reader.readLine();
+                connection.sendMessage(message);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
